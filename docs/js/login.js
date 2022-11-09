31e6c4447565099d442a3a1ef837e22db16f63f8
manesakae <manesakae@gmail.com>
2022-11-09 13:20:53 -0600

    fixes in submit form

--- docs/js/login.js
+++ docs/js/login.js
@@ -1,15 +1,13 @@
 "use strict";
-
-const loginForm = document.getElementById("login");
-loginForm.addEventListener("submit", loginSubmit, false);
-
-function loginSubmit(event) {
-    console.log("email: ", document.getElementById("email").value);
-    console.log("password: ", document.getElementById("password").value);
-    console.log("website: ", document.getElementById("website").value);
-    event.preventDefault();
-}
-
+window.onload = function () {
+    const loginForm = document.getElementById("login");
+    loginForm.addEventListener("submit", function(event) {
+        console.log("email: ", document.getElementById("email").value);
+        console.log("password: ", document.getElementById("password").value);
+        console.log("website: ", document.getElementById("website").value);
+        event.preventDefault();
+    });
+};
 
 
 
--- docs/js/product.js
+++ docs/js/product.js
@@ -1,16 +1,16 @@
 "use strict";
+window.onload = function() {
+    const productForm = document.getElementById("product");
+    productForm.addEventListener("submit", function(event) {
+        alert(`
+        Product Number: ${document.getElementById("productNumber").value}
+        Quantity in stock: ${document.getElementById("quantity").value}
+        Name: ${document.getElementById("name").value}
+        Supplier: ${document.getElementById("supplier").value}
+        Unit Price: ${document.getElementById("unitPrice").value}
+        Date supplied: ${document.getElementById("date").value}
+        `);
+        event.preventDefault();
+    });
+};
 
-const productForm = document.getElementById("product");
-productForm.addEventListener("submit", productSubmit, false);
-
-function productSubmit(event) {
-    alert(`
-    Product Number: ${document.getElementById("productNumber").value}
-    Quantity in stock: ${document.getElementById("quantity").value}
-    Name: ${document.getElementById("name").value}
-    Supplier: ${document.getElementById("supplier").value}
-    Unit Price: ${document.getElementById("unitPrice").value}
-    Date supplied: ${document.getElementById("date").value}
-    `);
-    event.preventDefault();
-}
